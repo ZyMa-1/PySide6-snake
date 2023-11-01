e55fb6964c11ffb85df1f6c3ee9efc4dbc968073
ZyMa-1 <57665203+ZyMa-1@users.noreply.github.com>
2023-11-01 22:30:15 +0600

    Documentation - SnakePainterConfig.py

--- SnakeGameWidget/SnakeCore/SnakePainterConfig.py
+++ SnakeGameWidget/SnakeCore/SnakePainterConfig.py
@@ -9,14 +9,19 @@ class SnakePainterConfig:
     ----------
     outer_border_color : QColor
         The color of the outer border of the game field.
+
     grass_cell_color : QColor
         The color of the grass cells in the game field.
+
     fruit_cell_color : QColor
         The color of the fruit cells in the game field.
+
     snake_cell_color : QColor
         The color of the snake body cells in the game field.
+
     snake_head_cell_color : QColor
         The color of the snake's head cell in the game field.
+
     outer_border_thickness : int
         The thickness of the outer border surrounding the game field.
     """
